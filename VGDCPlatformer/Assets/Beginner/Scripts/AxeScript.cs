78b3c1beda20c607fea95be5d0ef07fb0f088b11
Andy Kuei <akuei9000@gmail.com>
2018-10-26 20:16:28 -0700

    Axe?

---
+++ VGDCPlatformer/Assets/Beginner/Scripts/AxeScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeScript : MonoBehaviour {
+	public GameObject ThrownAxe;
+
+	public Rigidbody2D rb;
+	private float Velocity;
+	public string test;
+
+	void Start () {
+		//rb = GetComponent<Rigidbody2D>();
+		Debug.Log("Testing this " + test);
+		Velocity = 1f;
+		rb.AddForce(new Vector2(Velocity,Velocity));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
