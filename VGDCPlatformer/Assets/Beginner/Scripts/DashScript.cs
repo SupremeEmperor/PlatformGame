57122d74fe090f0423bd816a459fcbcb684e8fbb
Rigel Kuper <rkuper@uci.edu>
2018-11-01 10:40:55 -0700

    made scene for rigel and dash script

---
+++ VGDCPlatformer/Assets/Beginner/Scripts/DashScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashScript : MonoBehaviour {
+
+    public float dashDistance;
+    public float dashSpeed;
+    public float dashCoolDown;
+    private bool isDashing;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Dash()
+    {
+        yield return null;
+    }
+}
