0a89d9bc2916e0faea09931e1c10e071e9083346
Rigel Kuper <rkuper@uci.edu>
2018-11-09 18:26:22 -0800

    Created the play button to go to the level 1 scene

---
+++ VGDCPlatformer/Assets/Beginner/Scripts/Some Scripts/SceneManagement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        //======================= Important ===========================
+        //this does not work in play test mode(when u hit the play button in unity)
+        //this only works once we build the game and run it, then the quit button will work
+        Debug.Log("Quit game!");
+        Application.Quit();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
