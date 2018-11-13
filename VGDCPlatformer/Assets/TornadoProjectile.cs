dc4ebf77ea56e00b2a8aaee396db7af01b893fea
Fireryrex <victorwz@uci.edu>
2018-11-13 11:40:21 -0800

    limited number of tornados throwable

--- VGDCPlatformer/Assets/TornadoProjectile.cs
+++ VGDCPlatformer/Assets/TornadoProjectile.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TornadoProjectile : MonoBehaviour {
+    private ThrowWeapon Throwscript;
+    public GameObject Player;
+    public string ObjectName;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        Player = GameObject.Find(ObjectName);
+        Throwscript = Player.gameObject.GetComponent<ThrowWeapon>();
 	}
 	
 	// Update is called once per frame
@@ -15,4 +19,9 @@ public class TornadoProjectile : MonoBehaviour {
     {
         Destroy(gameObject, 5);
 	}
+
+    private void OnDestroy()
+    {
+        Throwscript.tornadocount -= 1;
+    }
 }
