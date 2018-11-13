f88b0ad022cc0719920e6725a097e56ce6e6f976
Kevin Touch <ktouch@uci.edu>
2018-11-13 11:06:06 -0800

    score made to count down

--- VGDCPlatformer/Assets/Beginner/Scripts/Score.cs
+++ VGDCPlatformer/Assets/Beginner/Scripts/Score.cs
@@ -4,22 +4,13 @@ using UnityEngine;
 public class Score : MonoBehaviour {
 
     public Text timer;
-    //public Text scoreText;
-    //public Text hiScoreText;
-
     public float timerCount = 300;
-    public float hiScoreCount;
     public float pointsPerSecond;
-
-    public bool scoreDecreasing;
-
-
-
     
     // Update is called once per frame
 	void Update () {
 
-        if (scoreDecreasing)
+        if (timerCount >= 1)
         {
             timerCount -= pointsPerSecond * Time.deltaTime; 
         }
@@ -30,10 +21,10 @@ public class Score : MonoBehaviour {
         //}
 
         string minutes = Mathf.Floor(timerCount / 60).ToString("00");
-        string seconds = (timerCount % 60).ToString("00");
+        string seconds = Mathf.Floor(timerCount % 60).ToString("00");
 
 
-        timer.text = "Time: " + string.Format("{0}:{1}", minutes,seconds);
+        timer.text = "Time: " + string.Format("{0}:{1}", minutes, seconds);
         //scoreText.text = "Score: " + Mathf.Round(scoreCount);
         //hiScoreText.text = "High Score: " + Mathf.Round(hiScoreCount);
 	}
