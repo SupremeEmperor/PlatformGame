401dbf5d1b1c7659019c20e929ece5183795cdb4
Fireryrex <victorwz@uci.edu>
2018-11-15 18:20:21 -0800

    changed some enemy scripts

--- VGDCPlatformer/Assets/TornadoEnemyProjectile.cs
+++ VGDCPlatformer/Assets/TornadoEnemyProjectile.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class TornadoEnemyProjectile : MonoBehaviour
 {
+    public int tornadoduration = 5;
+    //private EnemyMoving EnemyMovingScript;
+    //public string ObjectName;
+    //private GameObject Snail;
 
     // Use this for initialization
     void Start()
     {
-
+        //Snail = GameObject.Find(ObjectName);
+       // EnemyMovingScript = Snail.gameObject.GetComponent<EnemyMoving>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Destroy(gameObject, 5);
+        Destroy(gameObject, tornadoduration);
     }
+
+    /*private void OnDestroy()
+    {
+        Snail.GetComponent<EnemyMoving>().stopMove = false;
+    }*/
 }
