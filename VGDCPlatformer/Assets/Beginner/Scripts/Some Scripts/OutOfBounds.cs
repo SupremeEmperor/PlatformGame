ccbab897b6e1017d97e820427b679de301d139a7
Fireryrex <victorwz@uci.edu>
2018-11-27 12:57:07 -0800

    oob now resets level

--- VGDCPlatformer/Assets/Beginner/Scripts/Some Scripts/OutOfBounds.cs
+++ VGDCPlatformer/Assets/Beginner/Scripts/Some Scripts/OutOfBounds.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OutOfBounds : MonoBehaviour
 {
@@ -19,7 +20,9 @@ public class OutOfBounds : MonoBehaviour
     {
 		if (transform.position.y < OoB)
         {
-            transform.position = RespawnPointx.transform.position;
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+            //transform.position = RespawnPointx.transform.position;
             //HealthScript.HP -= 20;
         }
     }
