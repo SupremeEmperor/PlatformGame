6fd380cb348dbf7188be70a44d88046c4a8b53a5
Fireryrex <victorwz@uci.edu>
2018-12-04 10:55:04 -0800

    finalizing stuff

--- VGDCPlatformer/Assets/Beginner/Scripts/Some Scripts/SceneManagement.cs
+++ VGDCPlatformer/Assets/Beginner/Scripts/Some Scripts/SceneManagement.cs
@@ -13,6 +13,27 @@ public class SceneManagement : MonoBehaviour {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene("Level Select");
+    }
+    
+    public void LevelOne()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void LevelTwo()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void LevelThree()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+
+
     public void ExitGame()
     {
         //======================= Important ===========================
